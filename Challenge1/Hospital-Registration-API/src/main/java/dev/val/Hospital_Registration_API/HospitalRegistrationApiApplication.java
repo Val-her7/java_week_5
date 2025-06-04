f653f25300dbd80eef12579e0ff4d03072dcf28c
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-04 12:02:18 +0200

    Initial commit

---
+++ Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/HospitalRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package dev.val.Hospital_Registration_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ Challenge1/Hospital-Registration-API/src/test/java/dev/val/Hospital_Registration_API/HospitalRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.val.Hospital_Registration_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
