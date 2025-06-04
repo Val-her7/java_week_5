b256bc14695accc59808a0f3e4e61bb56845892d
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-04 12:31:38 +0200

    feat: add Doctor and Visit models

---
+++ Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Doctor.java
@@ -0,0 +1,33 @@
+package dev.val.Hospital_Registration_API.model;
+
+public class Doctor {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Doctor(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
