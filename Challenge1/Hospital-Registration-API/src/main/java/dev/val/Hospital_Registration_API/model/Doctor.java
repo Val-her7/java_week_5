78d93a55e74994f7267dde424df9bb1febb8f433
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-10 10:56:39 +0200

    refactor Doctor and Visit models witj JPA

--- Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Doctor.java
+++ Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Doctor.java
@@ -1,13 +1,29 @@
 package dev.val.Hospital_Registration_API.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "doctors")
 public class Doctor {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "first_name")
     private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
 }
--- Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Visit.java
+++ Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Visit.java
@@ -2,15 +2,38 @@ package dev.val.Hospital_Registration_API.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "visits")
 public class Visit {
 
-    private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
     private String lastName;
-    private String reasonOfRegistration;
+
+    @ManyToOne
+    @JoinColumn(name = "doctor_id")
+    private Doctor doctor;
+
     private LocalDateTime timestamp;
 }
