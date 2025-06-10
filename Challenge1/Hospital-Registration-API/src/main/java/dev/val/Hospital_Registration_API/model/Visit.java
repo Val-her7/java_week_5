8f1f5e5a7c225eb781ed31d165f74a592e45bf98
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-10 09:26:56 +0200

    feat: add Lombok dependency

--- Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Visit.java
+++ Challenge1/Hospital-Registration-API/src/main/java/dev/val/Hospital_Registration_API/model/Visit.java
@@ -2,42 +2,15 @@ package dev.val.Hospital_Registration_API.model;
 
 import java.time.LocalDateTime;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class Visit {
 
     private String name;
     private String lastName;
     private String reasonOfRegistration;
     private LocalDateTime timestamp;
-
-    public Visit(String name, String lastName, String reasonOfRegistration, LocalDateTime timestamp) {
-        this.name = name;
-        this.lastName = lastName;
-        this.reasonOfRegistration = reasonOfRegistration;
-        this.timestamp = timestamp;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getLastName() {
-        return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getReasonOfRegistration() {
-        return reasonOfRegistration;
-    }
-    public void setReasonOfRegistration(String reasonOfRegistration) {
-        this.reasonOfRegistration = reasonOfRegistration;
-    }
-    public LocalDateTime getTimestamp() {
-        return timestamp;
-    }
-    public void setTimestamp(LocalDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
 }
