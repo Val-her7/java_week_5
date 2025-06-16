8741127b92d1b7bdf8106d4e86c5165fbea49f04
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-16 14:32:23 +0200

    initial commit challenge 2

---
+++ Challenge2/API-Client/src/test/java/dev/val/API_Client/ApiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.val.API_Client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
