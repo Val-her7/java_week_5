9da45a06700c5c54e3360cdff061f3cbaf4ccaa0
Valher7 <Valentin.herman.pro@gmail.com>
2025-06-17 15:49:39 +0200

    Challenge3 initial commit

---
+++ Challenge3/data-analysis-tool/src/main/java/dev/val/data_analysis_tool/DataAnalysisToolApplication.java
@@ -0,0 +1,13 @@
+package dev.val.data_analysis_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAnalysisToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAnalysisToolApplication.class, args);
+	}
+
+}
---
+++ Challenge3/data-analysis-tool/src/test/java/dev/val/data_analysis_tool/DataAnalysisToolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.val.data_analysis_tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAnalysisToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
